3fae34129c6fe69cc74c5c1b6cccb84359e6b891
Matthew Born <me+code@mattborn.com>
2013-08-09 16:17:03 -0500

    Add accordion behavior

--- craigslist.js
+++ craigslist.js
@@ -1,3 +1,14 @@
 (function () {
-	//
+
+	$('.right-h2-link').click(function(){
+		var lists = $('.right-list'),
+			target = $(this).parent().next();
+		if (lists.is(':visible')) {
+			lists.slideUp();
+		}
+		if (!target.is(':visible')) {
+			target.slideDown();
+		}
+	});
+
 })(jQuery);
